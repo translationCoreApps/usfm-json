c096212d4280ba085e62410cceb043476671c528
RoyalSix <jay.a.scottjr@biola.edu>
2017-08-06 01:02:33 -0700

    Added support for handling half data

--- src/js/jsonToUsfm.js
+++ src/js/jsonToUsfm.js
@@ -12,18 +12,37 @@ exports.jsonToUSFM = function (json) {
   if (json.book) {
     final.push('\\h ' + json.book);
   }
-  for (var chapter in json) {
-    let chapterNumber = parseInt(chapter);
-    if (!chapterNumber || chapterNumber < 1) continue;
-    var currentChapter = json[chapterNumber];
-    final.push('\\c ' + chapterNumber);
-    final.push('\\p');
-    for (var verse in currentChapter) {
+  if (nestedLevel(json) == 2) {
+    for (var chapter in json) {
+      let chapterNumber = parseInt(chapter);
+      if (!chapterNumber || chapterNumber < 1) continue;
+      var currentChapter = json[chapterNumber];
+      final.push('\\c ' + chapterNumber);
+      final.push('\\p');
+      for (var verse in currentChapter) {
+        let verseNumber = parseInt(verse);
+        if (!verseNumber || verseNumber < 1) continue;
+        var currentVerse = currentChapter[verseNumber];
+        final.push('\\v ' + verseNumber + ' ' + currentVerse);
+      }
+    }
+  } else if (nestedLevel(json) == 1) {
+    for (var verse in json) {
       let verseNumber = parseInt(verse);
       if (!verseNumber || verseNumber < 1) continue;
-      var currentVerse = currentChapter[verseNumber];
+      var currentVerse = json[verseNumber];
       final.push('\\v ' + verseNumber + ' ' + currentVerse);
     }
   }
   return final.join('\n');
 };
+
+function nestedLevel(obj) {
+  var nestedLevel = 0; 
+  for (var element in obj) {
+    nestedLevel = 1;
+    if (typeof (obj[element]) === 'object')
+      nestedLevel = 2;
+  }
+  return nestedLevel;
+}

5cdae061753e25051193a9160e147e3d2d0366cb
RoyalSix <jay.a.scottjr@biola.edu>
2017-09-26 10:29:43 -0700

    added filter funciton with working regex

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var usfmToJson = require('./src/js/usfmToJson.js').usfmToJSON;
 var jsonToUsfm = require('./src/js/jsonToUsfm.js').jsonToUSFM;
 var getHeaders = require('./src/js/getHeaders.js');
+var filter = require('./src/js/filter');
 
 exports.toJSON = usfmToJson;
 exports.toUSFM = jsonToUsfm;
 exports.getHeaders = getHeaders;
+exports.removeMarker = filter.removeMarker;

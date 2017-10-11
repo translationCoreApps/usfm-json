1072a98823833f998fa7fc244a75ac19c50441ce
Joel Lonbeck <joel@neutrinographics.com>
2017-10-11 16:53:38 -0700

    added coverage and linting

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "extends": "google",
-    "rules": {
-        "max-len": ["error", { "ignoreStrings": true, ignoreComments:true }],
-        "no-unused-expressions":0
-    }
-};

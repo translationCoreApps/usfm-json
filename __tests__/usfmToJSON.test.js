69685be22a14bec1ad1f2992d637e39b05e51b59
Joel Lonbeck <joel@neutrinographics.com>
2017-10-13 13:47:59 -0700

    updated chunk parsing test

--- __tests__/usfmToJSON.test.js
+++ __tests__/usfmToJSON.test.js
@@ -4,13 +4,14 @@ import {usfmToJSON} from '../src/js/usfmToJson';
 /**
  * Generator for testing usfm to json migration
  * @param {string} name - the name of the test files to use. e.g. `valid` will test `valid.usfm` to `valid.json`
+ * @param {object} args - optional arguments to be passed to the converter
  */
-const generateTest = name => {
+const generateTest = (name, args = {}) => {
   const input = readUSFM(`${name}.usfm`);
   const expected = readJSON(`${name}.json`);
   expect(input).toBeTruthy();
   expect(expected).toBeTruthy();
-  const output = usfmToJSON(input);
+  const output = usfmToJSON(input, args);
   expect(output).toEqual(expected);
 };
 
@@ -32,4 +33,8 @@ it('handles missing chapter markers', () => {
 
 it('handles out of sequence chapter markers', () => {
   generateTest('out_of_sequence_chapters');
+});
+
+it('handles a chunk of usfm', () => {
+  generateTest('chunk', {chunk: true});
 });

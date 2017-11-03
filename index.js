cdc4beb4af90a7522f4c76581c0ef2a4060e8fce
RoyalSix <jay.a.scottjr@biola.edu>
2017-11-03 08:11:45 -0700

    init

--- index.js
+++
@@ -1,7 +0,0 @@
-import usfmToJSON from './src/js/usfmToJson';
-import jsonToUSFM from './src/js/jsonToUsfm';
-import removeMarker from './src/js/filter';
-
-exports.toJSON = usfmToJSON;
-exports.toUSFM = jsonToUSFM;
-exports.removeMarker = removeMarker;

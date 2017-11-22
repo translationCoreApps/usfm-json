275553f9ed43ab888c4efa04bf7d3a9e70f37f57
RoyalSix <jay.a.scottjr@biola.edu>
2017-11-22 02:03:56 -0500

    added updates to filter function

--- __tests__/removeMarker.test.js
+++ __tests__/removeMarker.test.js
@@ -3,13 +3,13 @@ import {removeMarker} from '../src/js/filter';
 const input = '...Christ \\q Jesus. \\f + \\ft Some early versions omit, \\fqa in Ephesus, \\fqa* but this expression is probably in Paul\'s original letter.\\f*';
 
 it('removes all extra tags', () => {
-  const expected = '...Christ Jesus. Some early versions omit, in Ephesus, but this expression is probably in Paul\'s original letter.';
+  const expected = '...Christ Jesus. ';
   const output = removeMarker(input);
   expect(output).toEqual(expected);
 });
 
 it('removes f tags', () => {
-  const expected = '...Christ \\q Jesus. Some early versions omit, in Ephesus, but this expression is probably in Paul\'s original letter.';
+  const expected = '...Christ \\q Jesus. ';
   const output = removeMarker(input, 'f');
   expect(output).toEqual(expected);
 });

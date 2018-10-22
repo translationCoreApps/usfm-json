cc3e448bf2891fa33c3632a0484ef1377dc7bbb6
Bruce McLean <mcleanb@photo-nomad.net>
2018-10-22 06:54:43 -0400

    fixes for white space on removeMarker and word markers.

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,5 +1,7 @@
-/* eslint-disable no-use-before-define */
+/* eslint-disable no-use-before-define,brace-style */
+import _ from "lodash";
 import {usfmToJSON} from './usfmToJson';
+import {jsonToUSFM} from './jsonToUsfm';
 
 /* Method to filter specified usfm marker from a string
  * @param {string} string - The string to remove specfic marker from
@@ -56,51 +58,153 @@ export const extractTextFromVerseObject = verseObject => {
 };
 
 /**
+ * dive down into milestone to extract words and text
+ * @param {Object} verseObject - milestone to parse
+ * @return {string} text content of milestone
+ */
+const parseMilestone = verseObject => {
+  let text = verseObject.text || "";
+  let wordSpacing = '';
+  const length = verseObject.children.length;
+  for (let i = 0; i < length; i++) {
+    let child = verseObject.children[i];
+    switch (child.type) {
+      case 'word':
+        text += wordSpacing + child.text;
+        wordSpacing = ' ';
+        break;
+
+      case 'milestone':
+        text += wordSpacing + parseMilestone(child);
+        wordSpacing = ' ';
+        break;
+
+      default:
+        if (child.text) {
+          text += child.text;
+          const lastChar = text.substr(-1);
+          if ((lastChar !== ",") && (lastChar !== '.') && (lastChar !== '?') && (lastChar !== ';')) { // legacy support, make sure padding before word
+            wordSpacing = '';
+          }
+        }
+        break;
+    }
+    if (child.nextChar) {
+      text += child.nextChar;
+    }
+  }
+  if (verseObject.nextChar) {
+    text += verseObject.nextChar;
+  }
+  return text;
+};
+
+/**
+ * get text from word and milestone markers
+ * @param {Object} verseObject - to parse
+ * @param {String} wordSpacing - spacing to use before next word
+ * @return {*} new verseObject and word spacing
+ */
+const replaceWordsAndMilestones = (verseObject, wordSpacing) => {
+  let text = '';
+  if (verseObject.type === 'word') {
+    text = wordSpacing + verseObject.text;
+  } else if (verseObject.children) {
+    text = wordSpacing + parseMilestone(verseObject);
+  }
+  if (text) { // replace with text object
+    verseObject = {
+      type: "text",
+      text
+    };
+    wordSpacing = ' ';
+  } else {
+    wordSpacing = ' ';
+    if (verseObject.nextChar) {
+      wordSpacing = ''; // no need for spacing before next word if this item has it
+    }
+    else if (verseObject.text) {
+      const lastChar = verseObject.text.substr(-1);
+      if (![',', '.', '?', ';'].includes(lastChar)) { // legacy support, make sure padding before next word if punctuation
+        wordSpacing = '';
+      }
+    }
+    if (verseObject.children) { // handle nested
+      const verseObject_ = _.cloneDeep(verseObject);
+      let wordSpacing_ = '';
+      const length = verseObject.children.length;
+      for (let i = 0; i < length; i++) {
+        const flattened =
+          replaceWordsAndMilestones(verseObject.children[i], wordSpacing_);
+        wordSpacing_ = flattened.wordSpacing;
+        verseObject_.children[i] = flattened.verseObject;
+      }
+      verseObject = verseObject_;
+    }
+  }
+  return {verseObject, wordSpacing};
+};
+
+/**
+ * convert verseObjects to Text
+ * @param {Array} verseObjects - to convert
+ * @return {string} converted text
+ */
+export const verseObjectsToText = verseObjects => {
+  const outputData = {
+    chapters: {},
+    headers: [],
+    verses: {
+      1: verseObjects
+    }
+  };
+  const USFM = jsonToUSFM(outputData, {chunk: true});
+  let split = USFM.split("\\v 1 ");
+  if (split.length <= 1) {
+    split = USFM.split("\\v 1");
+  }
+  return split.length > 1 ? split[1] : "";
+};
+
+/**
  * @description merge verse data into a string
  * @param {Object|Array} verseData - verse objects to be merged
  * @param {array} filter - Optional filter to get a specific type of word object type.
  * @return {String} - the merged verse object string
  */
 export const mergeVerseData = verseData => {
-  const verseArray = [];
-  let length = verseData.length;
-  for (let i = 0; i < length; i++) {
-    const part = verseData[i];
-    if (typeof part === 'string') {
-      verseArray.push(part);
-      continue;
-    }
-    let verseObjects = [part];
-    if (part.children) {
-      verseObjects = extractTextFromVerseObject(part);
-    }
-    const voLength = verseObjects.length;
-    for (let w = 0; w < voLength; w++) {
-      const word = verseObjects[w];
-      if (word.text) {
-        verseArray.push(word.text);
-      }
-    }
-    if (part.nextChar) {
-      verseArray.push(part.nextChar);
-    }
+  if (verseData.verseObjects) {
+    verseData = verseData.verseObjects;
   }
-  let verseText = '';
-  length = verseArray.length;
+  const flattenedData = [];
+  if (Array.isArray(verseData)) {
+    let wordSpacing = '';
+    const length = verseData.length;
+    for (let i = 0; i < length; i++) {
+      const verseObject = verseData[i];
+      const flattened = replaceWordsAndMilestones(verseObject, wordSpacing);
+      wordSpacing = flattened.wordSpacing;
+      flattenedData.push(flattened.verseObject);
+    }
+    verseData = { // use flattened data
+      verseObjects: flattenedData
+    };
+  }
+  let verseText = "";
+  let length = flattenedData.length;
   for (let i = 0; i < length; i++) {
-    const verse = verseArray[i];
-    if (verse) {
-      if (verseText) {
-        // make sure words aren't crammed together
-        const lastChar = verseText.substr(verseText.length - 1);
-        if ((lastChar !== '\n') && (lastChar !== ' ')) {
-          const nextChar = verse[0];
-          if ((nextChar !== '\n') && (nextChar !== ' ')) {
-            verseText += ' ';
-          }
+    const verseObj = flattenedData[i];
+    if (verseObj.text) {
+      if (verseObj.tag) {
+        const lastChar = verseText && verseText[verseText.length - 1];
+        if (!['', ' ', '\n'].includes(lastChar)) {
+          verseText += ' ';
         }
       }
-      verseText += verse;
+      verseText += verseObj.text;
+    }
+    if (verseObj.nextChar) {
+      verseText += verseObj.nextChar;
     }
   }
   return verseText;
